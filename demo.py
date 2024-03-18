9bb28ef42fa030ca57410b0b58374bfb8bcfea13
ayyoob150 <ayyoob150@gmail.com>
2024-03-18 16:14:42 +0530

    somthing new

--- demo.py
+++ demo.py
@@ -127,5 +127,84 @@ nasteddist = {'php':{'name':"abb"},'php2':{'name':"abb2"},'php3':{'name':"abb3"}
 #         print("hey there how are you")
 # constr = Constructors()
 
+# Inheritance
+# class A:
+#     def funA(self):
+#         print('a')
+# class B(A):
+#     def funB(self):
+#         print('b')
+# objB = B()
+# objB.funB()
+# objB.funA()
 
 
+# getter and setter
+# class Student:
+#     def __init__(self,con):
+#         self.__name=con
+#     def getter(self):
+#         print(self.__name)
+#     def setter(self,name):
+#         self.__name = name
+#
+# objsetget = Student('hettt')
+# objsetget.getter()
+# objsetget.setter('bye')
+# objsetget.getter()
+
+# Encapsulation (private , can not be directly use)
+# class Encap:
+#     __name = 'name'
+#     def __init__(self):
+#         print(self.__name)
+#         self.__priFun()
+#     def __priFun(self):
+#         print("hiii")
+# objE = Encap()
+# objE.__priFun() // error does not have __priFun attribute
+# print(objE.__name) // error does not have name attribute
+
+
+# polymorphism one thing several form --------------------------------------
+# class Shape:
+#     def area(self):
+#         raise NotImplementedError("Subclass must implement abstract method")
+#
+# class Rectangle(Shape):
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#
+#     def area(self):
+#         return self.length * self.width
+#
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
+#
+#     def area(self):
+#         return 3.14 * self.radius * self.radius
+#
+# # Function that calculates the area of any shape
+# def calculate_area(shape):
+#     return shape.area()
+#
+# # Creating instances of different subclasses
+# rectangle = Rectangle(5, 4)
+# circle = Circle(3)
+#
+# # Calling the function with different objects
+# print(calculate_area(rectangle))  # Output: 20
+# print(calculate_area(circle))     # Output: 28.26 (approximately)
+# overriding can resolve using super function
+# class A1:
+#     def abc(self):
+#         print('hi')
+# class A2(A1):
+#     def abc(self):
+#         super().abc()
+#         print('bye')
+#
+# obj = A2()
+# obj.abc()
